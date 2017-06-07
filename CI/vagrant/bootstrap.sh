64c9777c556abef41b5d449ede769c0d63924d3f
benlush <blush@blsystems.ca>
2017-06-07 07:14:05 -0700

    vagrant install ansible
    install latest stable version of ansible

--- CI/vagrant/bootstrap.sh
+++ CI/vagrant/bootstrap.sh
@@ -5,3 +5,5 @@ apt-get update
 # install python (https://gist.github.com/jbergantine/8742830)
 # install nginx (http://vegibit.com/how-to-provision-nginx-using-vagrant)
 apt-get install python-dev python-pip -q -y
+
+pip install ansible
