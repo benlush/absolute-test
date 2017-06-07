fd466b7456412f6f29e65f794ae237e8edd86dc3
benlush <blush@blsystems.ca>
2017-06-07 07:37:45 -0700

    use ansible to install,configure during bootstrap
    get the developers ready for making code changes

--- CI/vagrant/bootstrap.sh
+++ CI/vagrant/bootstrap.sh
@@ -3,7 +3,17 @@
 apt-get update
 
 # install python (https://gist.github.com/jbergantine/8742830)
-# install nginx (http://vegibit.com/how-to-provision-nginx-using-vagrant)
 apt-get install python-dev python-pip -q -y
 
+# install latest stable ansible
 pip install ansible
+
+if [ -e "absolute-test" ]; then
+    cd absolute-test/
+    git pull
+else
+    git clone https://github.com/benlush/absolute-test.git
+    cd absolute-test/
+fi
+
+ansible-playbook CI/playbooks/setup.yml --tags "install,configure"
