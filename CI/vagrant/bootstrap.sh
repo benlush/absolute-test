292c1c23313ad5da384753a89fdb9f3a9fe4d7ed
benlush <blush@blsystems.ca>
2017-06-07 07:10:11 -0700

    vagrant minimize
    ansible will take care of the sys config just ensure OS is ready for ansible

--- CI/vagrant/bootstrap.sh
+++ CI/vagrant/bootstrap.sh
@@ -4,7 +4,4 @@ apt-get update
 
 # install python (https://gist.github.com/jbergantine/8742830)
 # install nginx (http://vegibit.com/how-to-provision-nginx-using-vagrant)
-apt-get install python-dev python-pip nginx -q -y
-
-# install pip and uwsgi (https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-uwsgi-and-nginx-on-ubuntu-14-04)
-pip install virtualenv flask uwsgi
+apt-get install python-dev python-pip -q -y
